222ccd340b4622467ce27d6e71fd7ffb8b353ba3
Nao-Y1996 <60519909+Nao-Y1996@users.noreply.github.com>
2023-03-20 13:18:01 +0900

    refactoring

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import openai
 import redis
 import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
 
 from gpt_client import GPT
@@ -22,33 +23,33 @@ class File(BaseModel):
     path: str
 
 
-class SummarizeTarget(BaseModel):
-    value: str
+SYSTEM_SETTING = """わかりやすく答えてください"""
 
-
-SETTING_PROMPT = """わかりやすく答えてください"""
-
-summarize_settings = """
+SUMMARIZE_SYSTEM_SETTING = """
 ユーザーから渡される文章の要約をしてください。
 
 要約のフォーマットとしては「タイトル」「本文」「結論」の3つのパートに分けてください。
-ただし要約全体で1000字以内に納めてください
+ただし要約全体で400字以内に納めてください
 
 それでは文章を渡します。
 """
 
 
+@app.get("/")
+def redirect():
+    return RedirectResponse(url="/docs")
+
+
 @app.post("/")
 def gpt(use_message: UserMessage):
-    print(f"you : {use_message.value}")
     gpt = GPT()
+    # チャット履歴を取得する
     result = redis.zrange('chat', 0, -1, withscores=True)
     chat_length = len(result)
     # チャット履歴がない場合はChatGPTの振る舞い特性の初期プロンプトを設定する
     if chat_length == 0:
-        gpt.add_setting(SETTING_PROMPT)
+        gpt.add_setting(SYSTEM_SETTING)
         redis.zadd("chat", {json.dumps(gpt.latest_system_prompt()): chat_length})
-
     # チャットの履歴を取得してリクエストに含める
     for elm in result:
         past_message = json.loads(elm[0].decode("utf-8"))
@@ -60,7 +61,6 @@ def gpt(use_message: UserMessage):
     # チャットの返事をチャット履歴に保存する
     redis.zadd("chat", {json.dumps(gpt.latest_assistant_prompt()): chat_length + 1})
 
-    print(f"AI : {response}")
     # チャットの返事をレスポンスボディに入れて返す
     return {"response": response}
 
@@ -80,11 +80,10 @@ def transcript(file: File):
 
 
 @app.post("/summarize")
-def summarize(target: SummarizeTarget):
+def summarize(message: UserMessage):
     gpt = GPT()
     # ChatGPTの振る舞い特性を設定し、要約対象の文章ををリクエストに含めてリクエストを送る
-    summary = gpt.add_setting(summarize_settings).add_user_message(target.value).request()
-    print(summary)
+    summary = gpt.add_setting(SUMMARIZE_SYSTEM_SETTING).add_user_message(message.value).request()
     # 要約をレスポンスボディに入れて返す
     return {"response": summary}
 
